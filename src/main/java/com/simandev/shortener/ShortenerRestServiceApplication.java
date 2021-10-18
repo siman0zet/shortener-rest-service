f8fc76498bbce50ec3331867e632a09565ff48c5
siman0zet <siman3run@gmail.com>
2021-10-18 23:17:12 +1000

    Initial commit

---
+++ src/main/java/com/simandev/shortener/ShortenerRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.simandev.shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenerRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenerRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simandev/shortener/ShortenerRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simandev.shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenerRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
